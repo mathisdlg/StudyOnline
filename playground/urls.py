ae175f5ca5f35414cde73cf5bcd6598657a6e4d7
mathisdlg <delage.mathis.1@gmail.com>
2024-10-28 20:41:05 +0100

    Add some pages

--- playground/urls.py
+++ playground/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.say_hello),
+    path('', views.home, name='home'),
+    path('courses/', views.courses, name='courses'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('logout/', views.logout, name='logout'),
+    path('courses_inscriptions/<int:course_id>', views.courses_inscriptions, name='courses_inscriptions'),
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,52 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def say_hello(request):
+
+def home(request): # Check courses by category like prog, web dev, etc...
+    return render(request, 'home.html')
+
+
+def courses(request):
     courses = [
         {
+            "id": 1,
             "name": "Python",
+            "category": "Programming",
             "url": "https://www.edx.org/learn/python"
+        },
+        {
+            "id": 2,
+            "name": "Django",
+            "category": "Programming",
+            "url": "https://www.edx.org/learn/django"
+        },
+        {
+            "id": 3,
+            "name": "Flask",
+            "category": "Programming",
+            "url": "https://www.edx.org/learn/flask"
         }
     ]
-    return render(request, 'hello.html', {"name": "giga chad", "courses": courses})
+    return render(request, 'courses.html', {"courses": courses})
+
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'login.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def logout(request):
+    return render(request, 'home.html')
+
+def courses_inscriptions(request, course_id: int):
+    if request.user.is_authenticated:
+        print(request.user)
+        print("test")
+        return render(request, 'courses_inscriptions.html')
+    else:
+        print("test2")
+        return render(request, 'login.html')

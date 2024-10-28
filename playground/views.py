2fab4b1fbcebc306cb7d0b53cb6cc9957c371012
mathisdlg <delage.mathis.1@gmail.com>
2024-10-28 23:02:19 +0100

    Change categ

--- playground/views.py
+++ playground/views.py
@@ -17,13 +17,13 @@ def courses(request):
         {
             "id": 2,
             "name": "Django",
-            "category": "Programming",
+            "category": "Web development",
             "url": "https://www.edx.org/learn/django"
         },
         {
             "id": 3,
             "name": "Flask",
-            "category": "Programming",
+            "category": "Web development",
             "url": "https://www.edx.org/learn/flask"
         }
     ]
@@ -33,14 +33,18 @@ def courses(request):
 def login(request):
     return render(request, 'login.html')
 
+
 def register(request):
     return render(request, 'register.html')
+
 
 def profile(request):
     return render(request, 'profile.html')
 
+
 def logout(request):
     return render(request, 'home.html')
+
 
 def courses_inscriptions(request, course_id: int):
     if request.user.is_authenticated:
